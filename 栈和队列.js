e1e0f95ffea217848085c8a684bc7f21fac8a4b6
websf <276314445@qq.com>
2021-04-23 23:28:09 +0800

    栈和队列

--- 栈和队列.js
+++ 栈和队列.js
@@ -1,3 +1,35 @@
-function A(){
-    
+//栈
+function Stack(){
+    this.arr = [];
+    this.push = function(value){
+        this.arr.push(value);
+    }
+    this.pop = function(){
+        this.arr.pop();
+    }
 }
+var stack = new Stack();
+stack.push(1)
+stack.push(2)
+stack.push(3)
+console.log(stack.arr)
+stack.pop()
+console.log(stack.arr)
+
+//队列
+function Queue(){
+    this.arr = [];
+    this.push = function(value){
+        this.arr.push(value);
+    }
+    this.pop = function(){
+        this.arr.shift()
+    }
+}
+var queue = new Queue();
+queue.push(1)
+queue.push(2)
+queue.push(3)
+console.log(queue.arr)
+queue.pop()
+console.log(queue.arr)
