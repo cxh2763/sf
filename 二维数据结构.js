4432d35c64bd98ab251ce593c248172de6a2c241
websf <276314445@qq.com>
2021-04-25 10:36:30 +0800

    二维数据结构

--- 二维数据结构.js
+++ 二维数据结构.js
@@ -7,3 +7,31 @@ for(var i=0;i < arr.length; i++){
 }
 console.log(arr)
 //二维拓扑结构
+function Node(value){
+    this.value = value;
+    this.neighbor = [];
+}
+
+var a = new Node("a");
+var b = new Node("b");
+var c = new Node("c");
+var d = new Node("d");
+var e = new Node("e");
+var f = new Node("f");
+
+a.neighbor.push(b)
+a.neighbor.push(c)
+a.neighbor.push(f)
+
+b.neighbor.push(a)
+b.neighbor.push(d)
+b.neighbor.push(e)
+
+c.neighbor.push(a)
+
+d.neighbor.push(b)
+
+e.neighbor.push(b)
+
+f.neighbor.push(a)
+
